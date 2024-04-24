b8ece8349b2c9008580c0309c55667d0e3a993e9
Pabasara Navod <144713553+Pabs-codes@users.noreply.github.com>
2024-04-24 09:37:49 +0530

    build with cutom server.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    APP_NAME: process.env.APP_NAME,
-  },
+  output:"export",
+  // reactStrictMode: true,
+  // env: {
+  //   APP_NAME: process.env.APP_NAME,
+  // },
 }
 
 module.exports = nextConfig
